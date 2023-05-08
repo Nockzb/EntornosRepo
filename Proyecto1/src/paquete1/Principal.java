e99ec81c5580f62e014f010e046e4342ddd0ed34
DAM1 <DAM1@192.168.7.130>
2023-05-08 17:47:34 +0200

    Commit ejercicio 4.10 - 3

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Estamos haciendo control de versiones con Git");	
+		System.out.println("Estamos haciendo control de versiones con Git");		
+		
+		System.out.println("Prueba ejercicio 3 - tarea 4.10");
 	}
 
 }
