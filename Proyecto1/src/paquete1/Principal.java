b90f9650a53c141cc1d8810b507bbaff82e94d2e
DAM1 <DAM1@192.168.7.130>
2023-05-09 17:05:02 +0200

    .

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -6,7 +6,6 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Estamos haciendo control de versiones con Git");		
 		
-		System.out.println("Prueba ejercicio 3 - tarea 4.10");
+		System.out.println("Prueba ejercicio 3 - tarea 4.10");		
 	}
-
 }
